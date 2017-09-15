c9ae9874280b922c70df038cc97244934dbeb775
yellowtd <ren_amon@hotmail.com>
2017-09-14 23:18:21 -0400

    ignore transmap file

--- src/r_draw.c
+++ src/r_draw.c
@@ -222,7 +222,10 @@ void R_InitTranslationTables(void)
 	W_ReadLump(W_GetNumForName("TRANS70"), transtables+0x60000);
 	W_ReadLump(W_GetNumForName("TRANS80"), transtables+0x70000);
 	W_ReadLump(W_GetNumForName("TRANS90"), transtables+0x80000);
-	W_ReadLump(W_GetNumForName("TRANSA0"), transtables+0x90000);
+
+	size_t transa0 = W_CheckNumForName("TRANSA0");
+	if (transa0 != LUMPERROR)
+		W_ReadLump(transa0, transtables+0x90000);
 #endif
 }
 
