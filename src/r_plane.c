1092153d5c56865c2d7477e35f7fe1d7cc4dac61
Jaime Passos <lazymyuutsu@gmail.com>
2019-08-27 21:10:32 -0300

    Fix portal rendering

--- src/r_plane.c
+++ src/r_plane.c
@@ -286,22 +286,18 @@ void R_MapPlane(INT32 y, INT32 x1, INT32 x2)
 
 		if (span != 0)
 		{
-			INT32 xstep, ystep;
 #ifdef SOFTPOLY
 			if (bigstretchy)
 			{
-				INT64 den = (INT64)FRACUNIT * FRACUNIT * span;
-				xstep = (fixed_t)((INT64)planesin * planeheight * viewfocratio / den);
-				ystep = (fixed_t)((INT64)planecos * planeheight * viewfocratio / den);
+				ds_xstep = cachedxstep[y] = FixedMul(FixedMul(planesin, planeheight), viewfocratio) / span;
+				ds_ystep = cachedystep[y] = FixedMul(FixedMul(planecos, planeheight), viewfocratio) / span;
 			}
 			else
 #endif // SOFTPOLY
 			{
-				xstep = FixedMul(planesin, planeheight) / span;
-				ystep = FixedMul(planecos, planeheight) / span;
+				ds_xstep = cachedxstep[y] = FixedMul(planesin, planeheight) / span;
+				ds_ystep = cachedystep[y] = FixedMul(planecos, planeheight) / span;
 			}
-			ds_xstep = cachedxstep[y] = xstep;
-			ds_ystep = cachedystep[y] = ystep;
 		}
 	}
 	else
